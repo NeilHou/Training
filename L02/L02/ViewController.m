7d3b3baea24e0229e6d175fdd9100fa74ad1764c
NeilHou <houjianlei@gmail.com>
2015-05-17 13:35:18 +0800

    add l02 test

---
+++ L02/L02/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  L02
+//
+//  Created by Jianlei Hou on 5/17/15.
+//  Copyright (c) 2015 Jianlei Hou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
