73d70b559f7553d222829be11f829f7c13af4423
NeilHou <houjianlei@gmail.com>
2015-05-18 10:42:57 +0800

    create project l03

---
+++ L03/L03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L03
+//
+//  Created by Jianlei Hou on 5/18/15.
+//  Copyright (c) 2015 Jianlei Hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L03/L03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L03
+//
+//  Created by Jianlei Hou on 5/18/15.
+//  Copyright (c) 2015 Jianlei Hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
